a3bf46c0dcd3f030e4bed80440d39123291ffa9d
Misaka-L <lipww1234@foxmail.com>
2023-08-27 21:20:24 +0800

    fix: move smooth

--- Packages/com.vrcau.flatradar/Runtime/Scripts/TouchMap.cs
+++ Packages/com.vrcau.flatradar/Runtime/Scripts/TouchMap.cs
@@ -1,13 +1,26 @@
-﻿using UdonSharp;
+﻿using System;
+using UdonSharp;
 using UnityEngine;
+using VRC.Udon.Common.Enums;
 
 namespace FlatRadar
 {
     [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
     public class TouchMap : UdonSharpBehaviour
     {
+        private BoxCollider _collider;
+        private Rigidbody _rigidbody;
+        public GameObject screen;
+
+        private void Start()
+        {
+            _collider = GetComponent<BoxCollider>();
+            _rigidbody = GetComponent<Rigidbody>();
+        }
+
         public override void PostLateUpdate()
         {
+            _rigidbody.velocity = Vector3.zero;
             var gm = gameObject;
 
             var localPosition = gm.transform.localPosition;
@@ -15,6 +28,10 @@ namespace FlatRadar
             
             gm.transform.localPosition = localPosition; 
             gm.transform.localRotation = Quaternion.identity;
+            
+            var toScreenCenter = screen.transform.localPosition - transform.localPosition;
+            toScreenCenter.z = 0;
+            _collider.center = toScreenCenter;
         }
     }
 }
