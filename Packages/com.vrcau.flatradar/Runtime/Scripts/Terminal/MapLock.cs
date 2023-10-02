39c9e4b023bc584c77b0700c00a418205fd9a582
Misaka-L <lipww1234@foxmail.com>
2023-10-03 00:29:14 +0800

    feature: map lock

---
+++ Packages/com.vrcau.flatradar/Runtime/Scripts/Terminal/MapLock.cs
@@ -0,0 +1,24 @@
+﻿using JetBrains.Annotations;
+using UdonSharp;
+using UnityEngine.UI;
+
+namespace FlatRadar.Terminal
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class MapLock : UdonSharpBehaviour
+    {
+        public ScrollRect scrollRect;
+        public Toggle lockToggle;
+
+        private void Start()
+        {
+            lockToggle.isOn = false;
+        }
+
+        [PublicAPI]
+        public void _UpdateLock()
+        {
+            scrollRect.enabled = !lockToggle.isOn;
+        }
+    }
+}
